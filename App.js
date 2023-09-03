c21c2765dbf67d9debb59bc47f44d124375831e7
vimaladarshan <vimaladarshan@gmail.com>
2023-09-03 15:50:53 +0530

    changed from createElement to JSX

--- App.js
+++ App.js
@@ -1,33 +1,8 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from React"
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-/*
-<div id="parent">
-  <div id="child1">
-      <h1>h1 tag here</h1>
-      <h2>h2 tag here</h2>
-  </div>
-  <div id="child2">
-      <h1>h1 tag here</h1>
-      <h2>h2 tag here</h2>
-  </div>
- </div>
- */
+const heading = <h1 id="heading">Hello react!</h1>;
 
-const messTags = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "h1 tag here"),
-    React.createElement("h2", {}, "h2 tag here"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "h1 tag here"),
-    React.createElement("h2", {}, "h2 tag here"),
-  ]),
-]);
-
-console.log(messTags);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(messTags);
+
+root.render(heading);
