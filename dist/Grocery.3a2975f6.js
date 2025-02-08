6dd69a5e05abd12e16c643ee086d0f8be83742fa
Vimaladarshan <vimaladarshan@gmail.com>
2025-02-08 13:13:29 +0530

    move dist to prod

---
+++ dist/Grocery.3a2975f6.js
@@ -0,0 +1,2 @@
+var e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire94c2;(0,e.register)("7vyOq",function(n,o){Object.defineProperty(n.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(n.exports,"default",{get:function(){return i},set:void 0,enumerable:!0,configurable:!0});var r=e("5aRSq"),i=()=>/*#__PURE__*/(0,r.jsx)("h1",{children:"Grocery page"})});
+//# sourceMappingURL=Grocery.3a2975f6.js.map
